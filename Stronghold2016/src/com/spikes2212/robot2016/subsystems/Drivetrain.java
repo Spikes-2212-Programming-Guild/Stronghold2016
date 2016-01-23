d7720e2da1c7469425210adc0f40f6dbf7dbfa02
itamargreen <itamar.green@gmail.com>
2016-01-23 19:47:09 +0200

    added drivetrain

---
+++ Stronghold2016/src/com/spikes2212/robot2016/subsystems/Drivetrain.java
@@ -0,0 +1,39 @@
+package com.spikes2212.robot2016.subsystems;
+
+import com.spikes2212.robot2016.util.Gearbox;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drivetrain extends Subsystem {
+	Gearbox right, left;
+
+	public Drivetrain(Gearbox left, Gearbox right) {
+		this.right = right;
+		this.left = left;
+	}
+
+	public Drivetrain() {
+	}
+
+	public void straight(double speed) {
+		left.set(-speed);
+		right.set(speed);
+	}
+
+	public void turn(double speed) {
+		left.set(speed);
+		right.set(speed);
+	}
+
+	public double getLeftSpeed() {
+		return left.getSpeed();
+	}
+
+	public double getRightSpeed() {
+		return right.getSpeed();
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+	}
+}
