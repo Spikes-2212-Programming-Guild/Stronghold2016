5dbeb293dcf1e3e70dcf582f324c1e151227eb1e
Alon Gurny <alongurny@gmail.com>
2016-01-27 10:25:41 +0200

    Added JoystickMoveFolder

---
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/roller/folder/JoystickMoveFolder.java
@@ -0,0 +1,39 @@
+package com.spikes2212.robot2016.commands.roller.folder;
+
+import static com.spikes2212.robot2016.Robot.folder;
+
+import com.spikes2212.robot2016.commands.drivetrain.SpeedSupplier;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class JoystickMoveFolder extends Command {
+
+	private SpeedSupplier speed;
+
+	public JoystickMoveFolder(SpeedSupplier speed) {
+		requires(folder);
+		this.speed = speed;
+	}
+
+	protected void initialize() {
+	}
+
+	protected void execute() {
+		folder.moveFolder(speed.getSpeed());
+	}
+
+	protected boolean isFinished() {
+		return false;
+	}
+
+	protected void end() {
+		folder.stop();
+	}
+
+	protected void interrupted() {
+		end();
+	}
+}
