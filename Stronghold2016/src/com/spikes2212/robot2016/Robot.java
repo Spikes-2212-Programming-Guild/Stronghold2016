853233d23bca81b0c0a61b58d9df702d8cc5323d
Alon Gurny <alongurny@gmail.com>
2016-01-27 12:34:09 +0200

    Added autonomous

--- Stronghold2016/src/com/spikes2212/robot2016/Robot.java
+++ Stronghold2016/src/com/spikes2212/robot2016/Robot.java
@@ -1,5 +1,10 @@
 package com.spikes2212.robot2016;
 
+import java.util.Optional;
+
+import com.spikes2212.robot2016.commands.advanced.Cross.Defense;
+import com.spikes2212.robot2016.commands.advanced.CrossAndDropAndReturn;
+import com.spikes2212.robot2016.commands.advanced.CrossAndReturn;
 import com.spikes2212.robot2016.subsystems.Drivetrain;
 import com.spikes2212.robot2016.subsystems.Triz;
 import com.spikes2212.robot2016.subsystems.roller.Folder;
@@ -9,9 +14,11 @@ import com.spikes2212.robot2016.util.Gearbox;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -32,6 +39,10 @@ public class Robot extends IterativeRobot {
 	public static Shooter shooter;
 	public static Gyro gyro;
 
+	SendableChooser defenseChooser;
+	SendableChooser autoChooser;
+	Optional<Command> autoCommand = Optional.empty();
+
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -49,6 +60,18 @@ public class Robot extends IterativeRobot {
 		shooter = new Shooter(RobotMap.SHOOTER_TALON_PORT);
 		picker = new Picker(RobotMap.PICKER_TALON_PORT, RobotMap.BALL_LIMIT_SWITCH_CHANNEL);
 		folder = new Folder(RobotMap.FOLDER_TALON_PORT);
+		defenseChooser = new SendableChooser();
+		defenseChooser.addDefault("Low Bar", Defense.LOW_BAR);
+		defenseChooser.addObject("Portcullis", Defense.PORTCULLIS);
+		defenseChooser.addObject("Cheval de Frise", Defense.CHEVAL_DE_FRISE);
+		defenseChooser.addObject("Moat", Defense.MOAT);
+		defenseChooser.addObject("Ramparts", Defense.RAMPARTS);
+		defenseChooser.addObject("Rough Terrain", Defense.ROUGH_TERRAIN);
+		defenseChooser.addObject("Rock Wall", Defense.ROCK_WALL);
+		autoChooser = new SendableChooser();
+		autoChooser.addDefault("Cross & Return", "CrossAndReturn");
+		autoChooser.addObject("Cross, Drop & Return", "CrossAndDropAndReturn");
+		autoChooser.addObject("Cross & Shoot", "CrossAndShoot");
 	}
 
 	/**
@@ -76,15 +99,24 @@ public class Robot extends IterativeRobot {
 	 * to the switch structure below with additional strings & commands.
 	 */
 	public void autonomousInit() {
-
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
-
-		// schedule the autonomous command (example)
+		try {
+			Defense defense = (Defense) defenseChooser.getSelected();
+			switch ((String) autoChooser.getSelected()) {
+			case "CrossAndReturn":
+				autoCommand = Optional.of(new CrossAndReturn(defense));
+				break;
+			case "CrossAndDropAndReturn":
+				autoCommand = Optional.of(new CrossAndDropAndReturn(defense));
+				break;
+			case "CrossAndShoot":
+				break;
+			default:
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		autoCommand.ifPresent(Command::start);
 	}
 
 	/**
@@ -95,10 +127,6 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
 	}
 
 	/**
