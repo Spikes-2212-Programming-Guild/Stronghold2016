e12e9dfc5befcd5565cc2e8143689fb1a45107b1
Spikes Programming Guild <spikes.no2212@gmail.com>
2016-01-28 18:11:36 +0200

    Folder's sensors added

--- Stronghold2016/src/com/spikes2212/robot2016/subsystems/roller/Folder.java
+++ Stronghold2016/src/com/spikes2212/robot2016/subsystems/roller/Folder.java
@@ -1,5 +1,6 @@
 package com.spikes2212.robot2016.subsystems.roller;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -9,17 +10,30 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Folder extends Subsystem {
 	private SpeedController motor;
+	DigitalInput open, closed;
 
-	public Folder(SpeedController motor) {
+	public Folder(SpeedController motor, DigitalInput open, DigitalInput closed) {
 		this.motor = motor;
+		this.open = open;
+		this.closed = closed;
 	}
 
-	public Folder(int motorChannel) {
-		this(new VictorSP(motorChannel));
+	public Folder(int motorChannel, int openLimitSwitch, int closedLimitSwitch) {
+		this(new VictorSP(motorChannel), new DigitalInput(openLimitSwitch),
+				new DigitalInput(closedLimitSwitch));
+
 	}
 
 	public void moveFolder(double speed) {
 		motor.set(speed);
+	}
+
+	public boolean isOpen() {
+		return open.get();
+	}
+
+	public boolean isClose() {
+		return closed.get();
 	}
 
 	public void stop() {
