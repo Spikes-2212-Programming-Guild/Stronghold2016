b41f26c22386c9aa46d820afb54cc60c2455f9dc
mantin2212 <noam.mantin@gmail.com>
2016-01-28 18:20:58 +0200

    Implemented CrossRockWall

--- Stronghold2016/src/com/spikes2212/robot2016/commands/advanced/CrossRockWall.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/advanced/CrossRockWall.java
@@ -2,6 +2,10 @@ package com.spikes2212.robot2016.commands.advanced;
 
 import com.spikes2212.robot2016.commands.advanced.Cross.Direction;
 import com.spikes2212.robot2016.commands.drivetrain.StraightDriveByDistance;
+import com.spikes2212.robot2016.commands.roller.folder.ExpandRoller;
+import com.spikes2212.robot2016.commands.roller.folder.RetractRoller;
+import com.spikes2212.robot2016.commands.triz.MoveTrizDown;
+import com.spikes2212.robot2016.commands.triz.MoveTrizUp;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -13,6 +17,9 @@ public class CrossRockWall extends CommandGroup {
 	public static final double DISTANCE = 0;
 	
     public CrossRockWall(Direction direction) {
+    	//TODO: Make sure that MoveTrizUp is longer than RetractRolller
+    	addParallel(new RetractRoller());
+    	addSequential(new MoveTrizUp());
     	addSequential(new StraightDriveByDistance(direction.getSpeedDirection() * DISTANCE));
     }
 }
