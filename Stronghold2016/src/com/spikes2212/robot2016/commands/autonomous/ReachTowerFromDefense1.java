c5e68090a60eef6843762f80782b989e743c8357
Alon Gurny <alongurny@gmail.com>
2016-01-29 21:15:25 +0200

    Edited ReachTowerFromDefense commands

--- Stronghold2016/src/com/spikes2212/robot2016/commands/autonomous/ReachTowerFromDefense1.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/autonomous/ReachTowerFromDefense1.java
@@ -1,14 +1,24 @@
 package com.spikes2212.robot2016.commands.autonomous;
 
-import com.spikes2212.robot2016.commands.drivetrain.TurnAndDrive;
+import com.spikes2212.robot2016.commands.drivetrain.PIDStraightDriveByDistance;
+import com.spikes2212.robot2016.commands.drivetrain.PIDTurnDriveByAngle;
 
-public class ReachTowerFromDefense1 extends TurnAndDrive {
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
+public class ReachTowerFromDefense1 extends CommandGroup {
+
+	public static final double FIRST_DISTANCE = 0;
 	public static final double ANGLE = 0;
-	public static final double DISTANCE = 0;
+	public static final double SECOND_DISTANCE = 0;
+	public static final double TIMEOUT = 0.5;
 
 	public ReachTowerFromDefense1() {
-		super(ANGLE, DISTANCE);
+		addSequential(new PIDStraightDriveByDistance(FIRST_DISTANCE));
+		addSequential(new WaitCommand(TIMEOUT));
+		addSequential(new PIDTurnDriveByAngle(ANGLE));
+		addSequential(new WaitCommand(TIMEOUT));
+		addSequential(new PIDStraightDriveByDistance(FIRST_DISTANCE));
 	}
 
 }
