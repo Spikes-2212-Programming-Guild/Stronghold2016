69e0fea7a44501b8989e2fb78027a0ea2d92f3f3
hubiruchi <yooonatan@gmail.com>
2016-02-01 20:08:54 +0200

    added moveTrizToPosition

---
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/triz/MoveTrizToPosition.java
@@ -0,0 +1,61 @@
+package com.spikes2212.robot2016.commands.triz;
+
+import com.spikes2212.robot2016.Robot;
+import com.spikes2212.robot2016.pid.PIDCommand;
+import com.spikes2212.robot2016.pid.PIDCalculator.AbsoluteTolerance;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class MoveTrizToPosition extends PIDCommand {
+	private final static double KP=0;
+	private final static double KI=0;
+	private final static double KD=0;
+	private static final double ABSOLUTE_TOLERANCE = 0;
+	private final double startDistance;
+	private double maximumOutput;
+    public MoveTrizToPosition(double distance) {
+    	super(KP, KI, KD, distance, new AbsoluteTolerance(ABSOLUTE_TOLERANCE));
+    	startDistance=distance;
+    	requires(Robot.triz);
+        
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+
+	@Override
+	public double getPIDInput() {
+		return Robot.triz.getPosition()-startDistance;
+	}
+
+	@Override
+	public void usePIDOutput(double output) {
+		if(output!=0){
+			maximumOutput= Math.max(maximumOutput, output);
+			output/=maximumOutput;
+		}
+		Robot.triz.move(output);
+	}
+}
