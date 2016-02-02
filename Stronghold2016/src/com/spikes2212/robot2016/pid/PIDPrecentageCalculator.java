718349018f789c94c30e8150ecb542cd6407021e
hubiruchi <yooonatan@gmail.com>
2016-02-02 17:08:40 +0200

    added PID using max speed

---
+++ Stronghold2016/src/com/spikes2212/robot2016/pid/PIDPrecentageCalculator.java
@@ -0,0 +1,98 @@
+package com.spikes2212.robot2016.pid;
+
+import com.spikes2212.robot2016.pid.PIDCalculator.Tolerance;
+
+public class PIDPrecentageCalculator {
+
+	public static final long DEFAULT_DT = 30;
+
+	public interface Tolerance {
+		boolean hasReached(double setpoint, double error);
+	}
+
+	public static class AbsoluteTolerance implements Tolerance {
+		private double value;
+
+		public AbsoluteTolerance(double value) {
+			this.value = value;
+		}
+
+		@Override
+		public boolean hasReached(double setpoint, double error) {
+			return Math.abs(error) < value;
+		}
+	}
+
+	public static class RelativeTolerance implements Tolerance {
+		private double value;
+
+		public RelativeTolerance(double value) {
+			this.value = value;
+		}
+
+		@Override
+		public boolean hasReached(double setpoint, double error) {
+			return Math.abs(error / setpoint) < value;
+		}
+	}
+
+	private double kp, ki, kd;
+	private double pValue, iValue, dValue;
+	private double setpoint;
+	private double error, prevError;
+	private double maxSpeed;
+	private Tolerance tolerance;
+
+	public PIDPrecentageCalculator(double maxSpeed, double kp, double ki, double kd) {
+		this.kp = kp;
+		this.ki = ki;
+		this.kd = kd;
+		this.maxSpeed=maxSpeed;
+	}
+
+	public double calculate(double input) {
+		prevError = error;
+		error = setpoint - input;
+		pValue = kp * error;
+		iValue += ki * error;
+		dValue = kd * (error - prevError);
+		return getResult();
+	}
+
+	public double getResult() {
+		return (pValue + iValue + dValue)/maxSpeed;
+	}
+
+	public double getP() {
+		return kp;
+	}
+
+	public double getI() {
+		return ki;
+	}
+
+	public double getD() {
+		return kd;
+	}
+
+	public void setPID(double p, double i, double d) {
+		this.kp = p;
+		this.ki = i;
+		this.kd = d;
+	}
+
+	public void setSetpoint(double setpoint) {
+		this.setpoint = setpoint;
+		this.error = setpoint;
+	}
+
+	public void setTolerance(Tolerance tolerance) {
+		this.tolerance = tolerance;
+	}
+
+	public boolean hasReached() {
+		return tolerance.hasReached(setpoint, error);
+	}
+
+}
+	
