2378b1455d70e3dc30ed3d3e2ee66f22ef2b6223
Alon Gurny <alongurny@gmail.com>
2016-02-06 14:20:42 +0200

    Added camera support

---
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/camera/FrontStream.java
@@ -0,0 +1,47 @@
+package com.spikes2212.robot2016.commands.camera;
+
+import static com.spikes2212.robot2016.Robot.cameras;
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ImageType;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class FrontStream extends Command {
+
+	private Image image;
+
+	public FrontStream() {
+		requires(cameras);
+		image = NIVision.imaqCreateImage(ImageType.IMAGE_RGB, 0);
+	}
+
+	@Override
+	protected void initialize() {
+		cameras.startFront();
+	}
+
+	@Override
+	protected void execute() {
+		cameras.getImage(image);
+		CameraServer.getInstance().setImage(image);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {
+
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+
+}
