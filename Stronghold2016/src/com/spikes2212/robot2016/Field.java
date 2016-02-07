f6287229e1292c961fd53627e1ca8aea4914727d
Tamar <Tamar@LAPTOP-59ANECSI>
2016-02-07 19:27:29 +0200

    field constants initialization

--- Stronghold2016/src/com/spikes2212/robot2016/Field.java
+++ Stronghold2016/src/com/spikes2212/robot2016/Field.java
@@ -24,30 +24,26 @@ public class Field {
 
 	public enum DefenseLocation {
 		k1, k2, k3, k4, k5;
-		
+
 	}
 
 	public enum Goal {
 		LOW, HIGH
 	}
-	
-	public static final double FIELD_WIDTH = 8.1026,
-			HORIZONTAL_DISTANCED_BETWEEN_OUTWER_WORKS_AND_TOWER = 2.258,
-			OUTER_WORKS_TOTAL_LENGTH = 6.752,
-			SINGLE_DEFNSE_LENGTH = OUTER_WORKS_TOTAL_LENGTH / 5;
-	
-	public static double getDistanceFromDefenseToTower(int slot){
-		double horizontalDistance = FIELD_WIDTH / 2 //location of tower
-										- SINGLE_DEFNSE_LENGTH * (slot - 0.5);
-		return Math.sqrt(horizontalDistance * horizontalDistance + 
-						HORIZONTAL_DISTANCED_BETWEEN_OUTWER_WORKS_AND_TOWER
-						* HORIZONTAL_DISTANCED_BETWEEN_OUTWER_WORKS_AND_TOWER);
+
+	public static final double FIELD_LENGTH = 7.94258, HORIZONTAL_DISTANCED_BETWEEN_OUTWER_WORKS_AND_TOWER = 2.258,
+			OUTER_WORKS_TOTAL_LENGTH = 6.752, SINGLE_DEFNSE_LENGTH = OUTER_WORKS_TOTAL_LENGTH / 5;
+
+	public static double getDistanceFromDefenseToTower(int slot) {
+		double horizontalDistance = FIELD_LENGTH / 2 // location of tower
+				- SINGLE_DEFNSE_LENGTH * (slot - 0.5);
+		return Math.sqrt(horizontalDistance * horizontalDistance + HORIZONTAL_DISTANCED_BETWEEN_OUTWER_WORKS_AND_TOWER
+				* HORIZONTAL_DISTANCED_BETWEEN_OUTWER_WORKS_AND_TOWER);
 	}
-	
-	public static double getAngleToTurnFromDefenseTowardsTower(int slot){
+
+	public static double getAngleToTurnFromDefenseTowardsTower(int slot) {
 		double totalDistance = getDistanceFromDefenseToTower(slot);
 		return Math.acos(HORIZONTAL_DISTANCED_BETWEEN_OUTWER_WORKS_AND_TOWER / totalDistance);
 	}
-	
-	
+
 }
