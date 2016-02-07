67a15db4226e3886eaaaf512ae3f11ab4d1592cd
Spikes Programming Guild <spikes.no2212@gmail.com>
2016-02-07 21:38:24 +0200

    Added constant editing and re wrote code according to 3316

--- Stronghold2016/src/com/spikes2212/robot2016/subsystems/Cameras.java
+++ Stronghold2016/src/com/spikes2212/robot2016/subsystems/Cameras.java
@@ -2,7 +2,9 @@ package com.spikes2212.robot2016.subsystems;
 
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.Image;
+import com.spikes2212.robot2016.commands.camera.UseRearImage;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.vision.USBCamera;
@@ -12,23 +14,32 @@ public class Cameras extends Subsystem {
 	private USBCamera front, rear;
 	private boolean frontOn, rearOn;
 	private final double VIEW_ANGLE_UP = 37.1;
-	private final double CAMERA_ANGLE = 21;
-	private final double TARGET_HEIGHT = 80;
+	private double CAMERA_ANGLE = 21;
+	private double TARGET_HEIGHT = 80;
 	private final double CAMERA_RESOLUTION_Y = 480;
 	private final double Y_ANGLE_PER_PIXEL = VIEW_ANGLE_UP / CAMERA_RESOLUTION_Y;
-	public final double AREA_MIN = 60;
+	public double AREA_MIN = 1;
 
-	public final NIVision.Range rRange = new NIVision.Range(254, 255);
-	public final NIVision.Range gRange = new NIVision.Range(254, 255);
-	public final NIVision.Range bRange = new NIVision.Range(254, 255);
+	public NIVision.Range rRange = new NIVision.Range(0, 255);
+	public NIVision.Range gRange = new NIVision.Range(220, 255);
+	public NIVision.Range bRange = new NIVision.Range(0, 255);
 
-	public final NIVision.ParticleFilterOptions2 options = new NIVision.ParticleFilterOptions2(0, 0, 1, 1);
-	public final NIVision.ParticleFilterCriteria2[] criteria = {
-			new NIVision.ParticleFilterCriteria2(NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA, AREA_MIN, 100, 0, 0) };
-
+	NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
+	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(
+			0, 0, 1, 1);
+	
 	public Cameras(USBCamera front, USBCamera rear) {
 		this.front = front;
 		this.rear = rear;
+		SmartDashboard.putNumber("rRange min", rRange.minValue);
+		SmartDashboard.putNumber("rRange max", rRange.maxValue);
+		SmartDashboard.putNumber("gRange min", gRange.minValue);
+		SmartDashboard.putNumber("gRange max", gRange.maxValue);
+		SmartDashboard.putNumber("bRange min", bRange.minValue);
+		SmartDashboard.putNumber("bRange max", bRange.maxValue);
+		SmartDashboard.putNumber("Area min", AREA_MIN);
+		SmartDashboard.putNumber("Camera angle", CAMERA_ANGLE);
+		SmartDashboard.putNumber("Target height", TARGET_HEIGHT);
 	}
 
 	public Cameras(String frontName, String rearName) {
@@ -71,22 +82,42 @@ public class Cameras extends Subsystem {
 			rear.getImage(image);
 		}
 	}
+	
+	public void updateConstants(){
+		rRange = new NIVision.Range((int)SmartDashboard.getNumber("rRange min"), (int)SmartDashboard.getNumber("rRange max"));
+		gRange = new NIVision.Range((int)SmartDashboard.getNumber("gRange min"), (int)SmartDashboard.getNumber("gRange max"));
+		bRange = new NIVision.Range((int)SmartDashboard.getNumber("bRange min"), (int)SmartDashboard.getNumber("bRange max"));
+		AREA_MIN = SmartDashboard.getNumber("Area min");
+		CAMERA_ANGLE = SmartDashboard.getNumber("Camera angle");
+		TARGET_HEIGHT = SmartDashboard.getNumber("Target height");
+	}
 
 	private Image image = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
 	private Image binary = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_U8, 0);
 	public double getAngleUp() {
+		updateConstants();
 		getImage(image);
+		criteria[0] = new NIVision.ParticleFilterCriteria2(
+				NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA, AREA_MIN,
+				100.0, 0, 0);
 		NIVision.imaqColorThreshold(binary, image, 255, NIVision.ColorMode.RGB, rRange, gRange, bRange);
-		NIVision.imaqParticleFilter4(binary, binary, criteria, options, null);
+		criteria[0].lower = 5;
+		int imaqError = NIVision.imaqParticleFilter4(binary, binary,
+				criteria, filterOptions, null);
 		int count = NIVision.imaqCountParticles(binary, 1);
+		SmartDashboard.putNumber("count", count);
 		double maxArea = 0;
 		int maxIndex = 0;
 		for (int index = 0; index < count; index++) {
-			double area = NIVision.imaqMeasureParticle(image, index, 0, NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA);
+			double area = NIVision.imaqMeasureParticle(binary, index, 0, NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA);
 			maxIndex = area > maxArea ? index : maxIndex;
+			maxArea = area > maxArea ? area : maxArea;
+			SmartDashboard.putNumber("area", area);
+			SmartDashboard.putNumber("max index", maxIndex);
+			SmartDashboard.putNumber("max area", maxArea);
 		}
 		if (count != 0) {
-			double height = NIVision.imaqMeasureParticle(image, maxIndex, 0, NIVision.MeasurementType.MT_FIRST_PIXEL_Y);
+			double height =CAMERA_RESOLUTION_Y -  NIVision.imaqMeasureParticle(binary, maxIndex, 0, NIVision.MeasurementType.MT_FIRST_PIXEL_Y);
 			SmartDashboard.putNumber("height (PX)", height);
 			SmartDashboard.putNumber("angle", CAMERA_ANGLE + Y_ANGLE_PER_PIXEL * height);
 			return CAMERA_ANGLE + Y_ANGLE_PER_PIXEL * height;
@@ -102,6 +133,7 @@ public class Cameras extends Subsystem {
 
 	@Override
 	protected void initDefaultCommand() {
+		setDefaultCommand(new UseRearImage(CameraServer.getInstance()::setImage));
 	}
 
 }
