629d4d032fd8afc7b806965c8ab6dcc77110d85b
Alon Gurny <alongurny@gmail.com>
2016-02-12 17:01:08 +0200

    Fixed ShootByVoltage

--- Stronghold2016/src/com/spikes2212/robot2016/commands/shooter/ShootByVoltage.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/shooter/ShootByVoltage.java
@@ -3,14 +3,17 @@ package com.spikes2212.robot2016.commands.shooter;
 import com.spikes2212.robot2016.commands.picker.RollIn;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class ShootByVoltage extends CommandGroup {
 
-	public static final double ACCELERATION_TIME = 3;
-	public static final double ROLL_IN_TIME = 0.5;
+	public static final double ACCELERATION_TIME = 5;
+	public static final double WAIT_TIME_BEFORE_ROLLING = 3;
+	public static final double ROLL_IN_TIME = 1;
 
 	public ShootByVoltage(double voltage) {
-		addSequential(new RotateShooterByVoltageAndTime(voltage, ACCELERATION_TIME));
+		addParallel(new RotateShooterByVoltageAndTime(voltage, ACCELERATION_TIME));
+		addSequential(new WaitCommand(WAIT_TIME_BEFORE_ROLLING));
 		addSequential(new RollIn(ROLL_IN_TIME));
 	}
 
