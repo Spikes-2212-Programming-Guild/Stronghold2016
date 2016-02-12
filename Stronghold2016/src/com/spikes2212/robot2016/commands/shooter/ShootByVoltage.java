fc98cd881265ee58fd85407dff6a0f7731d52c30
Alon Gurny <alongurny@gmail.com>
2016-02-12 17:02:41 +0200

    Changed times

--- Stronghold2016/src/com/spikes2212/robot2016/commands/shooter/ShootByVoltage.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/shooter/ShootByVoltage.java
@@ -7,9 +7,9 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class ShootByVoltage extends CommandGroup {
 
-	public static final double ACCELERATION_TIME = 5;
+	public static final double ACCELERATION_TIME = 6;
 	public static final double WAIT_TIME_BEFORE_ROLLING = 3;
-	public static final double ROLL_IN_TIME = 1;
+	public static final double ROLL_IN_TIME = 2;
 
 	public ShootByVoltage(double voltage) {
 		addParallel(new RotateShooterByVoltageAndTime(voltage, ACCELERATION_TIME));
