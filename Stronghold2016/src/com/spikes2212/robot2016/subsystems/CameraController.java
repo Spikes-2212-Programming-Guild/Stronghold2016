6d03bfee8eae364e764e37060cca7407f1528e5d
Alon Gurny <alongurny@gmail.com>
2016-02-18 18:00:16 +0200

    Added camera controller things

--- Stronghold2016/src/com/spikes2212/robot2016/subsystems/CameraController.java
+++ Stronghold2016/src/com/spikes2212/robot2016/subsystems/CameraController.java
@@ -1,30 +1,37 @@
 package com.spikes2212.robot2016.subsystems;
 
+import java.util.Optional;
+
 import com.ni.vision.NIVision.Image;
 
 import edu.wpi.first.wpilibj.vision.USBCamera;
 
 public class CameraController {
 
-	private USBCamera camera;
+	private Optional<USBCamera> camera;
 	private boolean on;
 
 	public CameraController(String name) {
-		this.camera = new USBCamera(name);
+		try {
+			USBCamera camera = new USBCamera(name);
+			this.camera = Optional.of(camera);
+		} catch (Exception e) {
+			this.camera = Optional.empty();
+		}
 	}
 
 	public void start() {
-		camera.startCapture();
+		camera.ifPresent(USBCamera::startCapture);
 		on = true;
 	}
 
 	public void stop() {
+		camera.ifPresent(USBCamera::stopCapture);
 		on = false;
-		camera.stopCapture();
 	}
 
 	public void getImage(Image image) {
-		camera.getImage(image);
+		camera.ifPresent(c -> c.getImage(image));
 	}
 
 	public boolean isOn() {
