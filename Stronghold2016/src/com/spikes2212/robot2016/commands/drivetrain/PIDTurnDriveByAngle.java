3c2995abe5fd0acbfcd9b8ba2bef06f340b43978
Alon Gurny <alongurny@gmail.com>
2016-02-18 18:42:02 +0200

    Added things to PID

--- Stronghold2016/src/com/spikes2212/robot2016/commands/drivetrain/PIDTurnDriveByAngle.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/drivetrain/PIDTurnDriveByAngle.java
@@ -1,5 +1,9 @@
 package com.spikes2212.robot2016.commands.drivetrain;
 
+import static edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.getNumber;
+import static edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.putNumber;
+
+import com.spikes2212.robot2016.Constants;
 import com.spikes2212.robot2016.Robot;
 import com.spikes2212.robot2016.pid.PIDCommand;
 
@@ -8,12 +12,17 @@ public class PIDTurnDriveByAngle extends PIDCommand {
 	private static final double KP = 1;
 	private static final double KI = 0;
 	private static final double KD = 0;
-	private static final double ABSOLUTE_TOLERANCE = 2; // degree
+	private static final double TOLERANCE = 2; // degree
 
 	private double initialAngle;
+	private double angle;
 
 	public PIDTurnDriveByAngle(double angle) {
-		super(KP, KI, KD, angle, ABSOLUTE_TOLERANCE);
+		this.angle = angle;
+		putNumber("PIDTurnDriveByAngle.KP", KP);
+		putNumber("PIDTurnDriveByAngle.KI", KI);
+		putNumber("PIDTurnDriveByAngle.KD", KD);
+		putNumber("PIDTurnDriveByAngle.TOLERANCE", TOLERANCE);
 		requires(Robot.drivetrain);
 	}
 
@@ -24,12 +33,16 @@ public class PIDTurnDriveByAngle extends PIDCommand {
 
 	@Override
 	public void usePIDOutput(double output) {
-		Robot.drivetrain.turn(output);
+		Robot.drivetrain.turn(output / Constants.MAX_LEFT_VELOCITY);
 	}
 
 	@Override
 	protected void initialize() {
 		initialAngle = Robot.drivetrain.getYawAngle();
+		getCalculator().setPID(getNumber("PIDTurnDriveByAngle.KP", KP), getNumber("PIDTurnDriveByAngle.KI", KI),
+				getNumber("PIDTurnDriveByAngle.KD", KD));
+		getCalculator().setTolerance(getNumber("PIDTurnDriveByAngle.TOLERANCE", TOLERANCE));
+		getCalculator().setSetpoint(getNumber("PIDStraightDriveByDistance.DISTANCE", angle - initialAngle));
 	}
 
 	@Override
