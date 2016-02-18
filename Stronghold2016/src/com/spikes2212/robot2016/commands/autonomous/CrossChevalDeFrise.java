934676448e1b74759d1d798b738a5f719da7a3a2
yush5 <yush5@DESKTOP-3M2E2K2>
2016-02-19 01:26:26 +0200

    backward direction added to crosspotcullis and to crosschevaldesrise

--- Stronghold2016/src/com/spikes2212/robot2016/commands/autonomous/CrossChevalDeFrise.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/autonomous/CrossChevalDeFrise.java
@@ -3,6 +3,7 @@ package com.spikes2212.robot2016.commands.autonomous;
 import com.spikes2212.robot2016.Field.Direction;
 import com.spikes2212.robot2016.commands.drivetrain.DriveUntilBump;
 import com.spikes2212.robot2016.commands.drivetrain.PIDStraightDriveByDistance;
+import com.spikes2212.robot2016.commands.drivetrain.PIDTurnDriveByAngle;
 import com.spikes2212.robot2016.commands.triz.MoveTrizDown;
 import com.spikes2212.robot2016.commands.triz.MoveTrizUp;
 
@@ -12,14 +13,25 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class CrossChevalDeFrise extends CommandGroup {
+	
+	public static final double ROTATE_ANGLE = 180;
+	public static final double FORWARD_DISTANCE = 0;
+	public static final double FORWARD_AFTER_LIFTING_DISTANCE = 0;
+	public static final double BACKWARD_DISTANCE = 0;
+	public static final double BACKWARD_AFTER_LIFTING_DISTANCE = 0;
+	
 
-	public static final double DISTANCE = 1;
-	public static final double SPEED = 0.3;
-
-	public CrossChevalDeFrise(/*Direction direction*/) {
-		addSequential(new MoveTrizDown());
-		addSequential(new DriveUntilBump(/*direction.getSpeedDirection()**/SPEED ));
-		addParallel(new MoveTrizUp());
-		addSequential(new PIDStraightDriveByDistance(DISTANCE));
+	public CrossChevalDeFrise(Direction direction) {
+		if(direction==Direction.FORWARD){
+			addSequential(new PIDStraightDriveByDistance(FORWARD_DISTANCE));
+			addSequential(new MoveTrizUp());
+			addSequential(new PIDStraightDriveByDistance(FORWARD_AFTER_LIFTING_DISTANCE));
+		}else{
+			addSequential(new MoveTrizDown());
+			addSequential(new PIDTurnDriveByAngle(ROTATE_ANGLE));
+			addSequential(new PIDStraightDriveByDistance(BACKWARD_DISTANCE));
+			addSequential(new MoveTrizUp());
+			addSequential(new PIDStraightDriveByDistance(BACKWARD_AFTER_LIFTING_DISTANCE));
+		}
 	}
 }
--- Stronghold2016/src/com/spikes2212/robot2016/commands/autonomous/CrossPortcullis.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/autonomous/CrossPortcullis.java
@@ -20,14 +20,24 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class CrossPortcullis extends CommandGroup {
 
 	public static final double ROTATE_ANGLE = 180;
-	public static final double DRIVE_TO_PORTCULLIS_DISTANCE = 0;
-	public static final double AFTER_LIFTING_PORTCULLIS_DISTANCE = 0;
+	public static final double FORWARD_DISTANCE = 0;
+	public static final double FORWARD_AFTER_LIFTING_DISTANCE = 0;
+	public static final double BACKWARD_DISTANCE = 0;
+	public static final double BACKWARD_AFTER_LIFTING_DISTANCE = 0;
+	
 
-	public CrossPortcullis(/*Direction direction*/) {
-		
-		addSequential(new PIDStraightDriveByDistance(DRIVE_TO_PORTCULLIS_DISTANCE ));
-		addSequential(new MoveTrizDown());
-		addSequential(new PIDStraightDriveByDistance(AFTER_LIFTING_PORTCULLIS_DISTANCE));
+	public CrossPortcullis(Direction direction) {
+		if(direction==Direction.FORWARD){
+			addSequential(new PIDStraightDriveByDistance(FORWARD_DISTANCE));
+			addSequential(new MoveTrizDown());
+			addSequential(new PIDStraightDriveByDistance(FORWARD_AFTER_LIFTING_DISTANCE));
+		}else{
+			addSequential(new MoveTrizUp());
+			addSequential(new PIDTurnDriveByAngle(ROTATE_ANGLE));
+			addSequential(new PIDStraightDriveByDistance(BACKWARD_DISTANCE));
+			addSequential(new MoveTrizDown());
+			addSequential(new PIDStraightDriveByDistance(BACKWARD_AFTER_LIFTING_DISTANCE));
+		}
 	}
 
 }
