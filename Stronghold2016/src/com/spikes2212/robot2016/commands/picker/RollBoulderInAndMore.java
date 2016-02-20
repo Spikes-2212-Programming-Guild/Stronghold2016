c2af9b1ccdffe1218bd32453210dcda0ca413d5f
Alon Gurny <alongurny@gmail.com>
2016-02-20 09:19:29 +0200

    Name change

--- Stronghold2016/src/com/spikes2212/robot2016/commands/picker/RollBoulderInAndMore.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/picker/RollBoulderInAndMore.java
@@ -2,10 +2,10 @@ package com.spikes2212.robot2016.commands.picker;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class RollBallInAndMore extends CommandGroup {
+public class RollBoulderInAndMore extends CommandGroup {
 	public final double TIME_AFTER_PICKING = 0.2;
 
-	public RollBallInAndMore() {
+	public RollBoulderInAndMore() {
 		addSequential(new RollBoulderIn());
 		addSequential(new RollIn(), 0.2);
 	}
