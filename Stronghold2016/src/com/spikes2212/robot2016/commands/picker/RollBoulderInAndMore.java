b33071477349f413c93d45c5c96b84131dbebf74
Alon Gurny <alongurny@gmail.com>
2016-02-20 09:21:30 +0200

    Added a "static" modifier

--- Stronghold2016/src/com/spikes2212/robot2016/commands/picker/RollBoulderInAndMore.java
+++ Stronghold2016/src/com/spikes2212/robot2016/commands/picker/RollBoulderInAndMore.java
@@ -3,10 +3,10 @@ package com.spikes2212.robot2016.commands.picker;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class RollBoulderInAndMore extends CommandGroup {
-	public final double TIME_AFTER_PICKING = 0.2;
+	public static final double TIME_AFTER_PICKING = 0.2;
 
 	public RollBoulderInAndMore() {
 		addSequential(new RollBoulderIn());
-		addSequential(new RollIn(), 0.2);
+		addSequential(new RollIn(), TIME_AFTER_PICKING);
 	}
 }
