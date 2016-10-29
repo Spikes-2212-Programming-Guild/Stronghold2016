e0406df82daab6405a98d8e2d4c5b9b8c7f2325c
yonatan <yonatan@yonatan-Lenovo-U41-70>
2016-10-29 18:22:02 +0300

    documentation

--- Stronghold2016/src/com/spikes2212/robot2016/util/FeedForward.java
+++ Stronghold2016/src/com/spikes2212/robot2016/util/FeedForward.java
@@ -12,8 +12,24 @@ public class FeedForward {
 
 	PIDCalculator pid;
 	private double maxV, maxA, maxD, setpoint;
+	/*
+	 * the algorithem work by breaking the movement to 3 parts *
+	 */
 	double dt1, dt2, dt3;
 
+	/**
+	 * 
+	 * @param p-
+	 *            kp
+	 * @param maxV
+	 *            max velocity of subsystem
+	 * @param maxA
+	 *            max acceleration of subsystem
+	 * @param maxD
+	 *            max deceleration of subsystem
+	 * @param setpoint
+	 *            the distance to go
+	 */
 	public FeedForward(double p, double maxV, double maxA, double maxD, double setpoint) {
 		this.setpoint = setpoint;
 		pid = new PIDCalculator();
@@ -22,7 +38,15 @@ public class FeedForward {
 		dt2 = (setpoint / maxV - maxV / (2 * maxA) - maxV / (2 * maxD));
 		dt3 = maxV / maxD;
 	}
-
+/**
+ * 
+ * @param velocity
+ * @param acceleration
+ * @param location
+ * @param expected
+ * @return value for speed controller
+ */
+	//TODO: check return type
 	public double getVoltage(double velocity, double acceleration, double location, double expected) {
 		pid.setSetpoint(expected);
 		double value = pid.calculate(location);
@@ -34,10 +58,9 @@ public class FeedForward {
 		if (dt2 < 0) {
 			return new double[] { 0, 0, 0 };
 		}
-
 		if (t < dt1) {
 			return new double[] { maxA * t * t / 2, maxA * t, maxA };
-		} else if (t > dt1 && t < dt1 + dt2) {
+		} else if (t >= dt1 && t < dt1 + dt2) {
 			return new double[] { ((maxA * dt1 * dt1) / 2) + (t - dt1) * maxV, maxV, 0 };
 		} else {
 			double location = ((maxA * dt1 * dt1) / 2) + ((dt2 - dt1) * maxV) + ((t - dt1 - dt2) * (-maxD) / 2);
